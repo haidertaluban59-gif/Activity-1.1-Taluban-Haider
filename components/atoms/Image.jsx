17dc38c260b0c5865ee677bdcca15d3a23ac5704
Haider Taluban <haidertaluban59@gmail.com>
2026-09-04 23:40:36 -0700

    Fix GitHub Pages links

--- components/atoms/Image.jsx
+++ components/atoms/Image.jsx
@@ -1,14 +1,8 @@
-const BASE_PATH = '/Activity-1.1-Taluban-Haider';
-
 export default function HeritageImage({ src, alt, className = '' }) {
-  const imageSrc = src.startsWith(BASE_PATH)
-    ? src
-    : `${BASE_PATH}${src}`;
-
   return (
     <img
       className={className}
-      src={imageSrc}
+      src={src}
       alt={alt}
     />
   );
