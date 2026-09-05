fa12c5b145197344398e68e4755332dfd1966287
John Paul <YOUR-johnpaulangala1@gmail.com>
2026-09-05 15:25:05 +0800

    Fix GitHub Pages image paths

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+
   basePath: '/Activity-1.1-Taluban-Haider',
   assetPrefix: '/Activity-1.1-Taluban-Haider/',
+
   images: {
     unoptimized: true,
   },
